b6e00b9d1347bb38132ee98b7deebfd116930fd7
dandaneee <azurite8311@gmail.com>
2021-09-14 15:06:34 +0900

    1 commit

---
+++ gitupload/src/main/java/edu/spring/boot/GituploadApplication.java
@@ -0,0 +1,16 @@
+package edu.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+@SpringBootApplication
+public class GituploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GituploadApplication.class, args);
+		System.out.println("출력");
+	}
+
+}
